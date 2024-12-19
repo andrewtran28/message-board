a9780f8a702ebc06de53f99cc06a7435fe83bcc7
andrewtran28 <tran.andrew@outlook.com>
2024-12-19 16:53:40 -0500

    Message detail find by ID

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,8 +1,9 @@
 const asyncHandler = require("express-async-handler");
 
-const getMessageById = asyncHandler(async (requestAnimationFrame, res, next) => {
+const getMessageById = asyncHandler(async (req, res, next) => {
   const messageId = req.params.messageId;
-  res.render("message", {title: "User Message: ", message});
+  const messageDetails = messages.find((message) => message.id === messageId)
+  res.render("message", {title: "User Message", messageDetails});
 })
 
 module.exports = { getMessageById };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,13 +1,16 @@
 const { Router } = require("express");
 const indexRouter = Router();
+// const indexController = require("../controllers/indexController");
 
 const messages = [
   {
+    id: 0,
     text: "Hi there!",
     user: "Amando",
     added: new Date()
   },
   {
+    id: 1,
     text: "Hello World!",
     user: "Charles",
     added: new Date()
@@ -23,13 +26,13 @@ indexRouter.get("/new", (req, res) => {
 })
 
 indexRouter.post("/new", (req, res) => {
-  messages.push( { text: req.body.text, user: req.body.sender, added: new Date() });
+  messages.push( { id: messages.length, text: req.body.text, user: req.body.sender, added: new Date() });
   res.redirect("/");
 })
 
-indexRouter.get("/message/:userName", (req, res) => {
-  const messageDetails = messages.find((message) => message.user === req.params.userName);
-  res.render("message", { title: "User Message", message: messageDetails});
+indexRouter.get("/message/:messageId", (req, res) => {
+  const messageDetails = messages.find((message) => message.id == req.params.messageId);
+  res.render("message", {title: "User Message", message: messageDetails});
 })
 
 module.exports = indexRouter;
