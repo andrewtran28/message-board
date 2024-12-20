4d49c2bd0eb29c7669263ca1fed779b1a6165e79
andrewtran28 <tran.andrew@outlook.com>
2024-12-20 12:13:38 -0500

    Stylize messageboard

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,24 +1,42 @@
 const { Router } = require("express");
 const indexRouter = Router();
-// const indexController = require("../controllers/indexController");
+
+function getFormattedDate() {
+  const date = new Date();
+
+  const options = { month: 'short' };
+  const month = new Intl.DateTimeFormat('en-US', options).format(date);
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const hours = date.getHours() % 12 || 12; // Convert to 12-hour format
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const ampm = date.getHours() < 12 ? 'AM' : 'PM';
+
+  const timezone = new Intl.DateTimeFormat('en-US', { timeZoneName: 'short' })
+    .format(date)
+    .split(' ')
+    .pop();
+
+  return `${month} ${day}, ${year} @ ${hours}:${minutes}${ampm} ${timezone}`;
+}
 
 const messages = [
   {
     id: 0,
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: getFormattedDate()
   },
   {
     id: 1,
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: getFormattedDate()
   }
 ];
 
 indexRouter.get("/", (req, res) => {
-  res.render("index", {title: "Mini Messageboard", messages: messages});
+  res.render("index", { messages: messages });
 })
 
 indexRouter.get("/new", (req, res) => {
@@ -26,7 +44,7 @@ indexRouter.get("/new", (req, res) => {
 })
 
 indexRouter.post("/new", (req, res) => {
-  messages.push( { id: messages.length, text: req.body.text, user: req.body.sender, added: new Date() });
+  messages.push( { id: messages.length, text: req.body.text, user: req.body.sender, added: getFormattedDate() });
   res.redirect("/");
 })
 
